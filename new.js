84d1da92098061d682c0f22a05c365e6bacfec37
otuyamicah <otuyamicah@gmail.com>
2021-06-30 13:07:03 +0100

    new files

---
+++ new.js
@@ -0,0 +1,7 @@
+var name = "Hello this is Micah"
+var age = 10
+console.log(name)
+
+
+age >= 18 ? console.log("An Adult") : console.log("a child") 
+// Ternary Operator 
--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+var canDrink = false
+var age = 10
+
+if (age >=18){
+    canDrink = true
+}
+
+if (canDrink){
+    console.log("he is over " + age + ", give him a bear") 
+}
+
+if (!canDrink){
+    console.log("please this person cannot drink alcohol")
+}
+
+
+// var johnIscrazy = "false"
+
+
+console.log(!false)
+
 
 function myfun() {
-    var UserName = prompt("Enter Your UserName")
+    var UserName = prompt("Enter Your UserName");
     var Age = prompt("Enter Your Age")
-
-
-
-
+   
     if (Age <= 17 ) {
 
         window.alert("you sumbmited that you are " + Age + " years old, " + UserName + ".");
@@ -19,7 +37,7 @@ function myfun() {
         window.alert(UserName + " is an adult.");
     }
 
-    if (Age >= 36 && Age <= 60) {
+    if (Age >= 36 && Age < 60) {
 
         window.alert("you sumbmited that you are " + Age + " years old, " + UserName + ".");
         window.alert(UserName + " is a matured person.");
@@ -32,6 +50,8 @@ function myfun() {
     }
 
     document.write("<h1>Finally</h1>")
+
+   
 
 
 
